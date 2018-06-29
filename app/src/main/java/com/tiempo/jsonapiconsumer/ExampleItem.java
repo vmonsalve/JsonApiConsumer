9134ea4fa0a1a502f7bd6fd1568cb25bc9d0b360
Vicente Monsalve (Programador) <vicente.monsalve@outlook.com>
2018-06-29 14:30:34 -0400

    Ya se trae y visualiza informacion desde una api de imagenes.

---
+++ app/src/main/java/com/tiempo/jsonapiconsumer/ExampleItem.java
@@ -0,0 +1,19 @@
+package com.tiempo.jsonapiconsumer;
+
+public class ExampleItem {
+    private String mImageUrl;
+    private String mCreator;
+    private int mLikes;
+
+    public ExampleItem(String imageUrl, String creator, int likes){
+        mImageUrl = imageUrl;
+        mCreator  = creator;
+        mLikes    = likes;
+    }
+
+    public String getImageUrl() { return mImageUrl; }
+
+    public String getCreator() { return mCreator; }
+
+    public int getLikesCount() { return mLikes; }
+}
